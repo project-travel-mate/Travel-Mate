5312ef36afcac4cee912028dca81f03dba492984
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-02-12 23:01:02 +0530

    database added

--- APIs/db/travel_mate.sql
+++ APIs/db/travel_mate.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Feb 06, 2016 at 09:59 AM
+-- Generation Time: Feb 12, 2016 at 05:30 PM
 -- Server version: 5.6.22-72.0-log
 -- PHP Version: 5.4.31
 
@@ -28,7 +28,6 @@ USE `csinseew_pg_tie`;
 -- Table structure for table `cities`
 --
 
-DROP TABLE IF EXISTS `cities`;
 CREATE TABLE IF NOT EXISTS `cities` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `city_name` varchar(100) NOT NULL,
@@ -45,7 +44,6 @@ CREATE TABLE IF NOT EXISTS `cities` (
 -- Table structure for table `city_facts`
 --
 
-DROP TABLE IF EXISTS `city_facts`;
 CREATE TABLE IF NOT EXISTS `city_facts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `city_id` int(11) NOT NULL,
@@ -59,7 +57,6 @@ CREATE TABLE IF NOT EXISTS `city_facts` (
 -- Table structure for table `city_images`
 --
 
-DROP TABLE IF EXISTS `city_images`;
 CREATE TABLE IF NOT EXISTS `city_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `city_id` int(11) NOT NULL,
@@ -70,10 +67,38 @@ CREATE TABLE IF NOT EXISTS `city_images` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `monuments`
+--
+
+CREATE TABLE IF NOT EXISTS `monuments` (
+  `monument_id` int(11) NOT NULL AUTO_INCREMENT,
+  `monument_name` varchar(200) NOT NULL,
+  `monument_description` text,
+  `monument_image` varchar(500) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  PRIMARY KEY (`monument_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monuments_estimote`
+--
+
+CREATE TABLE IF NOT EXISTS `monuments_estimote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `UID` varchar(40) NOT NULL,
+  `major` int(10) NOT NULL,
+  `minor` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
