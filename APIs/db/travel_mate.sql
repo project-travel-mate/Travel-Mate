4e8b156f4a0dc46b621154be4c539ac05b068365
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-03-07 02:14:14 +0530

    login changed to device ID

--- APIs/db/travel_mate.sql
+++ APIs/db/travel_mate.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Feb 12, 2016 at 05:30 PM
+-- Generation Time: Mar 06, 2016 at 08:42 PM
 -- Server version: 5.6.22-72.0-log
 -- PHP Version: 5.4.31
 
@@ -28,6 +28,7 @@ USE `csinseew_pg_tie`;
 -- Table structure for table `cities`
 --
 
+DROP TABLE IF EXISTS `cities`;
 CREATE TABLE IF NOT EXISTS `cities` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `city_name` varchar(100) NOT NULL,
@@ -44,6 +45,7 @@ CREATE TABLE IF NOT EXISTS `cities` (
 -- Table structure for table `city_facts`
 --
 
+DROP TABLE IF EXISTS `city_facts`;
 CREATE TABLE IF NOT EXISTS `city_facts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `city_id` int(11) NOT NULL,
@@ -57,6 +59,7 @@ CREATE TABLE IF NOT EXISTS `city_facts` (
 -- Table structure for table `city_images`
 --
 
+DROP TABLE IF EXISTS `city_images`;
 CREATE TABLE IF NOT EXISTS `city_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `city_id` int(11) NOT NULL,
@@ -70,6 +73,7 @@ CREATE TABLE IF NOT EXISTS `city_images` (
 -- Table structure for table `monuments`
 --
 
+DROP TABLE IF EXISTS `monuments`;
 CREATE TABLE IF NOT EXISTS `monuments` (
   `monument_id` int(11) NOT NULL AUTO_INCREMENT,
   `monument_name` varchar(200) NOT NULL,
@@ -85,6 +89,7 @@ CREATE TABLE IF NOT EXISTS `monuments` (
 -- Table structure for table `monuments_estimote`
 --
 
+DROP TABLE IF EXISTS `monuments_estimote`;
 CREATE TABLE IF NOT EXISTS `monuments_estimote` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `UID` varchar(40) NOT NULL,
@@ -99,15 +104,17 @@ CREATE TABLE IF NOT EXISTS `monuments_estimote` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(500) NOT NULL,
-  `email` varchar(500) NOT NULL,
-  `password` varchar(200) NOT NULL,
+  `device_id` varchar(500) NOT NULL,
+  `name` varchar(500) DEFAULT NULL,
+  `email` varchar(500) DEFAULT NULL,
   `active` int(11) NOT NULL DEFAULT '0',
   `mood` int(5) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `device_id` (`device_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
