6f784ac06bfc7b72813ea51b8c68694b02bcd963
swati4star <swati4star@gmail.com>
2016-03-08 00:14:29 +0530

    Start journey layout changed

--- app/src/main/java/tie/hackathon/travelguide/MapActivity.java
+++ app/src/main/java/tie/hackathon/travelguide/MapActivity.java
@@ -13,6 +13,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -180,6 +181,10 @@ public class MapActivity extends AppCompatActivity {
         if(item.getItemId() ==android.R.id.home)
             finish();
 
+        if(item.getItemId() == R.id.action_sort){
+
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -266,6 +271,14 @@ public class MapActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.map_menu, menu);
+
+        return true;
     }
 
 
