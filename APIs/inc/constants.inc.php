3645323cdb13d6a93aed2597b059705a81c085f5
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-03-14 21:20:33 +0530

    weather api added

---
+++ APIs/inc/constants.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author: prabhakar
+ * @Date:   2016-03-14 21:07:46
+ * @Last Modified by:   Prabhakar Gupta
+ * @Last Modified time: 2016-03-14 21:09:01
+ */
+
+// open weather map API key used to get the 
+// weather of city from city name
+define("OPENWEATHERMAP_API_KEY", "1aa19f7dca713b11c29ec2cda827994c");
--- APIs/inc/function.inc.php
+++ APIs/inc/function.inc.php
@@ -1,53 +1,70 @@
 <?php
-	
-	$app_code = "AJKnXv84fjrb0KIHawS0Tg";
-	$app_id = "DemoAppId01082013GAL";
-	
-	$echonest_api_key = '0BOM42L07CR77AZZJ';
-	
-	$websites = ['http://www.saharastar.com/','http://www.mumbai.grand.hyatt.com','http://www.tridenthotels.com/mumbai_bandra_kurla/index.asp','http://international.bawahotels.com'];
-	$phones = ['+912240851800','+912261427328','+912266761234','+912266727777'];
 
+$app_code = "AJKnXv84fjrb0KIHawS0Tg";
+$app_id = "DemoAppId01082013GAL";
+
+$echonest_api_key = '0BOM42L07CR77AZZJ';
+
+$websites = ['http://www.saharastar.com/','http://www.mumbai.grand.hyatt.com','http://www.tridenthotels.com/mumbai_bandra_kurla/index.asp','http://international.bawahotels.com'];
+$phones = ['+912240851800','+912261427328','+912266761234','+912266727777'];
+
+
+function getTimeStamp($time){
+	return date('Y-m-d h:i:s', $time);
+}
+
+function curl_URL_call($url){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
+}
+
+function call_here_api($params, $latitude, $longitude, $limit = 0){
+	$here_api_url = 'https://places.demo.api.here.com/places/v1/discover/explore?at=' . $latitude . '%2C' . $longitude . '&cat=' . $params . '&app_id=' .$GLOBALS['app_id'] . '&app_code=' . $GLOBALS['app_code'];
 	
-	function getTimeStamp($time){
-		return date('Y-m-d h:i:s', $time);
-	}
+	if($limit != 0)
+		$here_api_url .= '&size=' . $limit;
 	
-	function curl_URL_call($url){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-		$output = curl_exec($ch);
-		curl_close($ch);
-		return $output;
-	}
+	$response = json_decode(curl_URL_call($here_api_url), true);
+	return $response['results']['items'];
+}
+
+function getMonumentID($connection, $beacon_id){
+	$monument_id = 0;
 	
-	function call_here_api($params, $latitude, $longitude, $limit = 0){
-		$here_api_url = 'https://places.demo.api.here.com/places/v1/discover/explore?at=' . $latitude . '%2C' . $longitude . '&cat=' . $params . '&app_id=' .$GLOBALS['app_id'] . '&app_code=' . $GLOBALS['app_code'];
-		
-		if($limit != 0)
-			$here_api_url .= '&size=' . $limit;
-		
-		$response = json_decode(curl_URL_call($here_api_url), true);
-		return $response['results']['items'];
-	}
+	$query = "SELECT `id` FROM `monuments_estimote` WHERE `major`='$beacon_id' LIMIT 1";
+	$query_row = mysqli_fetch_assoc(mysqli_query($connection, $query));
+	$monument_id = (int)$query_row['id'];
 	
-	function getMonumentID($connection, $beacon_id){
-		$monument_id = 0;
-		
-		$query = "SELECT `id` FROM `monuments_estimote` WHERE `major`='$beacon_id' LIMIT 1";
-		$query_row = mysqli_fetch_assoc(mysqli_query($connection, $query));
-		$monument_id = (int)$query_row['id'];
-		
-		return $monument_id;
-	}
-	
-	function encryptText($text){
-		return strip_tags(addslashes($text));
-	}
-	
-	function decryptText($text){
-		return stripcslashes($text);
-	}
-	
+	return $monument_id;
+}
+
+function encryptText($text){
+	return strip_tags(addslashes($text));
+}
+
+function decryptText($text){
+	return stripcslashes($text);
+}
+
+/**
+ * function to get city weather
+ * from city name using openweathermap API
+ * @param  string	$city_name
+ * @return array
+ */
+function getCityWeather($city_name){
+	$url = "http://api.openweathermap.org/data/2.5/weather?q=" . $city_name . "&APPID=" . OPENWEATHERMAP_API_KEY;
+	$api_response = json_decode(curl_URL_call($url), true);
+
+	$return_array = array(
+		'min'	=> (float)$api_response['temp_min'],
+		'max'	=> (float)$api_response['temp_max'],
+	);
+
+	return $return_array;
+}
