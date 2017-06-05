36d031c2250d32b74554982240ca0dafdef0036c
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2017-06-05 19:22:36 +0530

    APIs: cities autocomplete query corrected

--- APIs/city/autocomplete.php
+++ APIs/city/autocomplete.php
@@ -5,7 +5,12 @@ require_once '../inc/connection.inc.php';
 $query_searched = trim($_GET['search']);
 $final_response = array();
 
-$query = "SELECT `id`,`city_name` FROM `cities` WHERE `city_name` LIKE '%$query_searched%' ORDER BY `city_name` ASC";
+$query = "SELECT `id`,`city_name` FROM `cities` WHERE `city_name` LIKE '%$query_searched%'
+	 ORDER BY CASE WHEN `city_name` like '$query_searched%' THEN 0
+               WHEN `city_name` like '%$query_searched%' THEN 1
+               ELSE 2
+          END, `city_name` LIMIT 10";
+
 $query_run = mysqli_query($connection, $query);
 
 while($query_row = mysqli_fetch_assoc($query_run)){
