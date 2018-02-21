c6f67cff1fc6dcbcf9521c3e1a05c6c8d633e2f2
Madalina Turcu <c_mturcu@groupon.com>
2018-02-21 17:20:46 +0200

    Fixed Espresso integration

--- Android/app/src/androidTest/java/tie/hackathon/travelguide/Tests/DemoTest.java
+++ Android/app/src/androidTest/java/tie/hackathon/travelguide/Tests/DemoTest.java
@@ -4,7 +4,12 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 /**
  * Created by valentin.boca on 2/21/2018.
@@ -15,5 +20,10 @@ public class DemoTest extends EspressoTestBase {
     @Test
     public void testDemo() throws Exception {
         onView(withContentDescription("SIGNUP")).perform(click());
+    }
+
+    @Test
+    public void testDemo2() throws Exception {
+        onView(allOf(withText("Travel Mate"), hasSibling(withContentDescription("Travel Guide")))).check(matches(isDisplayed()));
     }
 }
