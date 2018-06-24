ce7a9d1a47f81c06065bf822a9e4a0c0e90f5633
Pritom Mazumdar <pritommazumdar1995@gmail.com>
2018-06-24 11:51:35 +0530

    Update CityFragment.java

--- Android/app/src/main/java/io/github/project_travel_mate/destinations/CityFragment.java
+++ Android/app/src/main/java/io/github/project_travel_mate/destinations/CityFragment.java
@@ -19,7 +19,6 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
