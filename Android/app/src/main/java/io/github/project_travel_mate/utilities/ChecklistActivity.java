9f997c21688da3019c56d78806d820a73aa64f86
aayushi nigam <aayushinigam2109@gmail.com>
2018-06-30 12:38:28 +0530

    implemented room persistence lib for databas
    made suggested changes
    
    Fixed checkstyle issues
    
    fixed travis build errors

--- Android/app/src/main/java/io/github/project_travel_mate/utilities/ChecklistActivity.java
+++ Android/app/src/main/java/io/github/project_travel_mate/utilities/ChecklistActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 import java.util.Objects;
@@ -18,6 +19,7 @@ public class ChecklistActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
 
