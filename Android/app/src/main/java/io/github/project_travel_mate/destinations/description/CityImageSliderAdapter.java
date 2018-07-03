3392345423200005b1d6d9908232f840313d41bb
Syed Jahangir <syedjahangir.p16@iiits.in>
2018-07-03 10:48:42 +0530

    Fixes #135

---
+++ Android/app/src/main/java/io/github/project_travel_mate/destinations/description/CityImageSliderAdapter.java
@@ -0,0 +1,48 @@
+package io.github.project_travel_mate.destinations.description;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import io.github.project_travel_mate.R;
+
+public class CityImageSliderAdapter extends PagerAdapter {
+    private Context mContext;
+    private ArrayList<String> mImagesArray;
+    CityImageSliderAdapter(Context context, ArrayList<String> imagesArray) {
+        this.mContext = context;
+        this.mImagesArray = imagesArray;
+    }
+    @Override
+    public int getCount() {
+        return mImagesArray.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        Picasso.with(mContext).load(mImagesArray.get(position))
+                .error(R.drawable.delhi).fit().centerCrop().into(imageView);
+        container.addView(imageView);
+        return imageView;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
