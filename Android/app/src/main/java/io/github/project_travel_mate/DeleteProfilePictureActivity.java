e785e3ed2d89f6b8379133a6b00f70db15f77bf5
Syed Jahangir <syedjahangir.p16@iiits.in>
2018-08-03 18:20:52 +0530

    Allow user to remove profile photo #207

---
+++ Android/app/src/main/java/io/github/project_travel_mate/DeleteProfilePictureActivity.java
@@ -0,0 +1,19 @@
+package io.github.project_travel_mate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class DeleteProfilePictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("remove_image", true);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+    }
+}
