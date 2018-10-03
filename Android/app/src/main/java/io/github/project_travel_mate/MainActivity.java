64a458c7aa84bdfcc0c0731c01a8458e261f876e
Anastasios Moraitis <anas.moraitis@gmail.com>
2018-10-03 18:45:25 +0300

    Adds contributors to the project
    Improvements till now:
    	HttpRequest implemented only for android developers
    	AboutUs contributors' button connected to a new fragment
    	Basic UI elements added to the fragment
    	Adapters and model ready as we can from the first improvent

--- Android/app/src/main/java/io/github/project_travel_mate/MainActivity.java
+++ Android/app/src/main/java/io/github/project_travel_mate/MainActivity.java
@@ -105,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         whatsNew.presentAutomatically(this);
 
         // To check for shared profile intents
-        String action =  getIntent().getAction();
+        String action = getIntent().getAction();
         if (Intent.ACTION_VIEW.equals(action)) {
             showProfile(getIntent().getDataString());
         }
@@ -221,10 +221,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             }
 
-            case R.id.nav_myfriends :
+            case R.id.nav_myfriends:
                 fragment = MyFriendsFragment.newInstance();
                 break;
-            case R.id.nav_settings :
+            case R.id.nav_settings:
                 fragment = SettingsFragment.newInstance();
                 break;
         }
@@ -419,7 +419,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_notification :
+            case R.id.action_notification:
                 Intent intent = NotificationsActivity.getStartIntent(MainActivity.this);
                 startActivity(intent);
                 return true;
