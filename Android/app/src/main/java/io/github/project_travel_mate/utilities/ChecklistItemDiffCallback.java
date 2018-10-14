b72f1fda0c6e1c6aab861839a5e2a32c5c6b1300
C Shri Akhil <shriakhilc@gmail.com>
2018-10-15 01:55:46 +0530

    Checklist Revamp
    * RecyclerViews with DiffUtil, Drag & Drop
    * RxJava source Flowable(s) with distinctUntilChanged
    * Add TypeConverter for boolean storage
    * Add Migration code
    
    Closes #300

---
+++ Android/app/src/main/java/io/github/project_travel_mate/utilities/ChecklistItemDiffCallback.java
@@ -0,0 +1,39 @@
+package io.github.project_travel_mate.utilities;
+
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+
+import objects.ChecklistItem;
+
+public class ChecklistItemDiffCallback extends DiffUtil.Callback {
+
+    private final List<ChecklistItem> mOldList;
+    private final List<ChecklistItem> mNewList;
+
+    public ChecklistItemDiffCallback(List<ChecklistItem> oldItemsList, List<ChecklistItem> newItemsList) {
+        this.mOldList = oldItemsList;
+        this.mNewList = newItemsList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return mOldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return mNewList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        // items are the same if their "id" matches
+        return (mOldList.get(oldItemPosition).getId() == mNewList.get(newItemPosition).getId());
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return mOldList.get(oldItemPosition).equals(mNewList.get(newItemPosition));
+    }
+}
