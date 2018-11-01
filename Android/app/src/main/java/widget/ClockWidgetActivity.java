b6f758e708292eea5833d4479b24a932df215671
Joshua Jadulco <joshuajadulco@gmail.com>
2018-11-01 02:42:45 -0400

    Created prototype for the Word Clock Widget

---
+++ Android/app/src/main/java/widget/ClockWidgetActivity.java
@@ -0,0 +1,49 @@
+package widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import io.github.project_travel_mate.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+
+public class ClockWidgetActivity extends AppWidgetProvider {
+    private static DateFormat mCurrentTime = new SimpleDateFormat("hh:mm:ss");
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock_widget_activity);
+        views.setTextViewText(R.id.tView_digital_clock, mCurrentTime.format(new Date()));
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
