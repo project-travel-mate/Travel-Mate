cc3a94201d59a57223ca333b8a8c1403b378dd7d
Josh J <joshuajadulco@gmail.com>
2018-12-09 03:13:37 -0500

    World Clock Widget Config Added
    Added configuration screen for World Clock Widget to be shown on launcher/home screen. Also fixed the display for the time in the widget while displaying the timezone name properly.

--- Android/app/src/main/java/widget/ClockWidget.java
+++ Android/app/src/main/java/widget/ClockWidget.java
@@ -4,36 +4,26 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
 
-import java.text.DateFormat;
+import java.util.TimeZone;
 
 import io.github.project_travel_mate.R;
 
 /**
  * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link ClockWidgetConfigureActivity ClockWidgetConfigureActivity}
  */
+public class ClockWidget extends AppWidgetProvider {
 
-public class ClockWidgetActivity extends AppWidgetProvider {
-    private static DateFormat mCurrentTime = new SimpleDateFormat("hh:mm:ss");
-    private static Date mDateBasis = Calendar.getInstance().getTime();
-    private static DateFormat mCurrentDate = new SimpleDateFormat("dd/MM/yyyy");
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        String mTimezoneCode = ClockWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock_widget_activity);
-        //Set Time, Date, and TimeZone of the widget
-        TimeZone currentTZ = TimeZone.getDefault();
-        views.setTextViewText(R.id.tView_digital_clock, mCurrentTime.format(new Date()));
-        views.setTextViewText(R.id.tView_timezone, currentTZ.getDisplayName());
-        views.setTextViewText(R.id.tView_tzdate, mCurrentDate.format(mDateBasis));
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock_widget);
+        TimeZone targetTimezone = TimeZone.getTimeZone(mTimezoneCode);
+        views.setTextViewText(R.id.tView_timezone, targetTimezone.getID());
+        views.setString(R.id.txtClockDigitalClock, "setTimeZone",  mTimezoneCode);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
@@ -47,6 +37,14 @@ public class ClockWidgetActivity extends AppWidgetProvider {
     }
 
     @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (int appWidgetId : appWidgetIds) {
+            ClockWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
+        }
+    }
+
+    @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
     }
@@ -56,5 +54,4 @@ public class ClockWidgetActivity extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 }
-
 
