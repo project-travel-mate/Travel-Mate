cca13fcc47567c7544af8474f79344975b385893
Jaideep Prasad <jddeep003@gmail.com>
2019-03-24 20:46:44 +0530

    #585 - Added Hotels Fragment on home page(#586)

--- Android/app/src/main/java/io/github/project_travel_mate/HomeFragment.java
+++ Android/app/src/main/java/io/github/project_travel_mate/HomeFragment.java
@@ -2,15 +2,22 @@ package io.github.project_travel_mate;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.card.MaterialCardView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.project_travel_mate.travel.HotelsActivity;
 
 public class HomeFragment extends Fragment {
 
     private Activity mActivity;
+    @BindView(R.id.materialCardView2)
+    MaterialCardView mHotelBookingView;
 
     public HomeFragment() {
     }
@@ -24,6 +31,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, rootview);
+        mHotelBookingView.setOnClickListener(v -> {
+            Intent hotelIntent = HotelsActivity.getStartIntent(mActivity);
+            startActivity(hotelIntent);
+
+        });
         return rootview;
     }
 
