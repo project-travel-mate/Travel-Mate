51d4878077c0faa8c077048e259eadbfbe266bc0
Hein Khant Zaw <40730345+HeinKhantZaw@users.noreply.github.com>
2019-10-12 14:14:06 +0630

    Update TweetsActivity.java

--- Android/app/src/main/java/io/github/project_travel_mate/destinations/description/TweetsActivity.java
+++ Android/app/src/main/java/io/github/project_travel_mate/destinations/description/TweetsActivity.java
@@ -83,7 +83,7 @@ public class TweetsActivity extends AppCompatActivity {
         String uri = API_LINK_V2 + "get-city-trends/" + mCity.getId();
         Log.v("EXECUTING", uri);
 
-        //Set up Client
+        //Set up client
         OkHttpClient client = new OkHttpClient();
         //Execute request
         Request request = new Request.Builder()
