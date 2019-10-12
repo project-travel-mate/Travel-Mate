591fd7017ad4e9f9609cac99712cab78eba745bf
Hein Khant Zaw <40730345+HeinKhantZaw@users.noreply.github.com>
2019-10-12 14:53:37 +0630

    Update HangoutSpotFragment.java

--- Android/app/src/main/java/io/github/project_travel_mate/travel/swipefragmentrealtime/modefragments/HangoutSpotFragment.java
+++ Android/app/src/main/java/io/github/project_travel_mate/travel/swipefragmentrealtime/modefragments/HangoutSpotFragment.java
@@ -87,7 +87,7 @@ public class HangoutSpotFragment extends Fragment {
 
         Log.v("EXECUTING", uri);
 
-        //Set up Client
+        //Set up client
         OkHttpClient client = new OkHttpClient();
         //Execute request
         Request request = new Request.Builder()
