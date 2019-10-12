9f8c8540a5d21eb975ce16e68442689e5de3db40
Hein Khant Zaw <40730345+HeinKhantZaw@users.noreply.github.com>
2019-10-12 14:34:05 +0630

    Update MyFriendsFragment.java

--- Android/app/src/main/java/io/github/project_travel_mate/friend/MyFriendsFragment.java
+++ Android/app/src/main/java/io/github/project_travel_mate/friend/MyFriendsFragment.java
@@ -83,7 +83,7 @@ public class MyFriendsFragment extends Fragment {
 
         Log.v("EXECUTING", uri);
 
-        //Set up Client
+        //Set up client
         OkHttpClient client = new OkHttpClient();
         //Execute request
         final Request request = new Request.Builder()
