3b61fc6f7535a7ec66e822c375d85869e26d73ba
Hein Khant Zaw <40730345+HeinKhantZaw@users.noreply.github.com>
2019-10-12 14:48:28 +0630

    Update HotelsActivity.java

--- Android/app/src/main/java/io/github/project_travel_mate/travel/HotelsActivity.java
+++ Android/app/src/main/java/io/github/project_travel_mate/travel/HotelsActivity.java
@@ -204,7 +204,7 @@ public class HotelsActivity extends AppCompatActivity implements View.OnClickLis
         String uri = API_LINK_V2 + "get-all-cities/5000";
         Log.v("EXECUTING", uri);
 
-        //Set up Client
+        //Set up client
         OkHttpClient client = new OkHttpClient();
         //Execute request
         final Request request = new Request.Builder()
