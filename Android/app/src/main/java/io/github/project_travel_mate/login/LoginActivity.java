638c4fabcf315ed7ed4caa81df9399a93761fcad
Irfan <irfansa1@ymail.com>
2019-10-16 17:56:40 +0530

    Modified to use existing code from Utils.java for connectivity checking.

--- Android/app/src/main/java/io/github/project_travel_mate/login/LoginActivity.java
+++ Android/app/src/main/java/io/github/project_travel_mate/login/LoginActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
