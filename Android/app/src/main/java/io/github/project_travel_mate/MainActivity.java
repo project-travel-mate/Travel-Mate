738374c3dd04ca7d72a0211cb3038e511e552a32
Muhammad Hafiz <mhbt-703@hotmail.com>
2019-10-29 01:11:31 +0800

    Update MainActivity - removed unused import
    removed import butterknife.Optional;

--- Android/app/src/main/java/io/github/project_travel_mate/MainActivity.java
+++ Android/app/src/main/java/io/github/project_travel_mate/MainActivity.java
@@ -41,7 +41,7 @@ import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.Optional;
+
 import io.github.project_travel_mate.destinations.CityFragment;
 import io.github.project_travel_mate.favourite.FavouriteCitiesFragment;
 import io.github.project_travel_mate.friend.FriendsProfileActivity;
