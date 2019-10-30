1b86d19bcb956af2745d37bfb6f4d117b3c2512c
Fokion Sotiropoulos <fokion.s@gmail.com>
2019-10-30 20:35:38 +0000

    Adding tests with broken dates
    I chose the assertTrue as the DateUtils will give the current time and the comparison is happening within couple milliseconds and it is logical to be < 1000 (even on a really slow vm)

--- Android/app/src/test/java/utils/DateUtilsTest.java
+++ Android/app/src/test/java/utils/DateUtilsTest.java
@@ -4,10 +4,12 @@ package utils;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Locale;
-import java.util.logging.SimpleFormatter;
+import static org.junit.Assert.assertTrue;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,7 +18,12 @@ public class DateUtilsTest {
     @Test
     public void rfc3339ToMills() {
         assertEquals(1572374198000L , DateUtils.rfc3339ToMills("2019-10-29T18:36:38.000GMT").longValue());
+        long epoch = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+        assertTrue(((epoch - DateUtils.rfc3339ToMills("abc").longValue()) < 1000));
+        epoch = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+        assertTrue(((epoch - DateUtils.rfc3339ToMills("10-23-2001").longValue()) < 1000));
     }
+
     @Test
     public void getDate_with_mils() {
         assertEquals("29 Oct 2019" , DateUtils.getDate(1572374198000L));
